b3e1235af5b379de51f752f010413a4e5943f6f1
edenIsakov <31316799+edenIsakov@users.noreply.github.com>
2022-12-31 18:10:11 +0200

    fix version

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
+import os
+
 from setuptools import setup, find_packages
 
-__version__ = exec(open('src/fedex_generator/version.py').read())
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version():
+    for line in read('src/fedex_generator/__init__.py').splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+def get_long_description():
+    with open('README.md', 'r') as fh:
+        return fh.read()
+
 
 setup(
     name='fedex_generator',
-    version='0.0.1',
+    version=get_version(),
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
+    long_description=get_long_description(),  # Long description read from the readme file
     project_urls={
         'Git': 'https://github.com/analysis-bots/FEDEx_Generator',
     },
