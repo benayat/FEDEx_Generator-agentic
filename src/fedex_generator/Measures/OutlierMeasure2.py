868c571bf2120a57ca36c096632c4d7765da149e
itayely <itay.elyashiv@gmail.com>
2024-06-24 19:45:47 +0300

    added control group, better explanations

--- src/fedex_generator/Measures/OutlierMeasure2.py
+++ src/fedex_generator/Measures/OutlierMeasure2.py
@@ -32,7 +32,7 @@ class OutlierMeasure(BaseMeasure):
         df_final = df_in.copy()
         final_agg_df = None
         for p in preds:
-            attr, i, score, kind = p
+            attr, i, score, kind, rank = p
             if(kind == 'bin'):
                 bin = i
                 df_exc = df_final[((df_final[attr] < bin[0]) | (df_final[attr] >= bin[1]))]
@@ -41,7 +41,7 @@ class OutlierMeasure(BaseMeasure):
             agged_val = df_exc.groupby(g_att)[g_agg].agg(agg_method)
             inf = self.calc_influence_pred(df_agg, agged_val, target, dir)
             if inf/final_inf>1.1:
-                final_pred.append((attr, i))
+                final_pred.append((attr, i, rank))
                 final_inf = inf
                 df_final = df_exc
                 final_agg_df = agged_val
@@ -50,19 +50,24 @@ class OutlierMeasure(BaseMeasure):
         return final_pred, final_inf, final_agg_df
 
 
-    def explain_outlier(self, df_agg, df_in, g_att, g_agg, agg_method, target, dir, k=1):
+    def explain_outlier(self, df_agg, df_in, g_att, g_agg, agg_method, target, dir, control=None,k=1):
         attrs = df_in.select_dtypes(include='number').columns.tolist()#[:10]
         attrs = [a for a in attrs if a not in [g_att, g_agg]]
         exps = {}
         worst = 0
-        top_bin_all = None
+        # top_bin_all = None
         top_inf_all = -1
-        top_attr = None
+        # top_attr = None
         df = None
         predicates = []
+        if control == None:
+            control=list(df_agg.index)
+        df_in_consider = df_in[(df_in[g_att].isin(control))|(df_in[g_att] == target)]
+        df_agg_consider = df_agg[control]
+        df_agg_consider[target] = df_agg[target]
         for attr in attrs:
-            if attr=='duration_minutes':
-                pass
+            # if attr=='duration_minutes':
+            #     pass
             series = df_in[attr]
             dtype = df_in[attr].dtype.name
             flag = False
@@ -77,66 +82,71 @@ class OutlierMeasure(BaseMeasure):
                     top_bin = None
                     for i in vals.index:
                         # df_in_target_exc = df_in[((df_in[g_att].isin(target))&(df_in[attr] != i))]
-                        df_in_target_exc = df_in[(df_in[attr] != i)]
+                        df_in_target_exc = df_in_consider[(df_in_consider[attr] != i)]
                         agged_val = df_in_target_exc.groupby(g_att)[g_agg].agg(agg_method)
                         # agged_df = df_agg.copy()
                         # agged_df[target]=agged_val
-                        inf = self.calc_influence_pred(df_agg, agged_val,target, dir)#/(df_in[attr].count()/df_in_exc[attr].count())
+                        inf = self.calc_influence_pred(df_agg_consider, agged_val,target, dir)#/(df_in[attr].count()/df_in_exc[attr].count())
                         exps[(attr,i)]=inf
-                        if inf > top_inf:
-                            top_inf = inf
-                            top_bin = i
-                            top_df = agged_val
-                        predicates.append((attr, i, inf, 'cat'))
-                    if top_inf > top_inf_all:
-                        top_inf_all = top_inf
-                        top_bin_all = top_bin
-                        top_attr = attr
-                        df = top_df
+                        # if inf > top_inf:
+                        #     top_inf = inf
+                        #     top_bin = i
+                        #     top_df = agged_val
+                        predicates.append((attr, i, inf, 'cat', None))
+                    # if top_inf > top_inf_all:
+                    #     top_inf_all = top_inf
+                    #     top_bin_all = top_bin
+                    #     top_attr = attr
+                    #     df = top_df
                     
                             
             if not flag:
-                for n in [10]:
+                for n in [20]:
                     _, bins = pd.cut(series, n, retbins=True, duplicates='drop')
-                    df_bins_in = pd.cut(df_in[attr], bins=bins).value_counts(normalize=True).sort_index()#.rename('idx')
+                    df_bins_in = pd.cut(df_in_consider[attr], bins=bins).value_counts(normalize=True).sort_index()#.rename('idx')
                     top_df = None
                     top_inf = 0
                     top_bin = None
+                    i = 1
                     for bin in df_bins_in.keys():
                         # df_in_exc = df_in[((df_in[g_att].isin(target)) & ((df_in[attr] < bin.left) | (df_in[attr] >= bin.right)))]
                         new_bin = (float("{:.2f}".format(bin.left)), float("{:.2f}".format(bin.right)))
-                        df_in_exc = df_in[((df_in[attr] < new_bin[0]) | (df_in[attr] >= new_bin[1]))]
+                        df_in_exc = df_in_consider[((df_in_consider[attr] < new_bin[0]) | (df_in_consider[attr] >= new_bin[1]))]
                         agged_val = df_in_exc.groupby(g_att)[g_agg].agg(agg_method)
                         # agged_df = df_agg.copy()
                         # agged_df[target]=agged_val
-                        inf = self.calc_influence_pred(df_agg, agged_val, target, dir)#/np.sqrt(df_in[attr].count()/df_in_exc[attr].count())
+                        inf = self.calc_influence_pred(df_agg_consider, agged_val, target, dir)#/np.sqrt(df_in[attr].count()/df_in_exc[attr].count())
                         exps[(attr,(new_bin[0],new_bin[1]))]=inf
-                        if inf > top_inf:
-                            top_inf = inf
-                            top_bin = new_bin
-                            top_df = agged_val
-                        predicates.append((attr, new_bin, inf, 'bin'))
-                    if top_inf > top_inf_all:
-                        top_inf_all = top_inf
-                        top_bin_all = top_bin
-                        top_attr = attr
-                        df = top_df
+                        # if inf > top_inf:
+                        #     top_inf = inf
+                        #     top_bin = new_bin
+                        #     top_df = agged_val
+                        predicates.append((attr, new_bin, inf, 'bin', i))
+                        i += 1
+                    # if top_inf > top_inf_all:
+                    #     top_inf_all = top_inf
+                    #     top_bin_all = top_bin
+                    #     top_attr = attr
+                    #     df = top_df
             # if top_inf_all >= 1:
             #             break
         predicates.sort(key=lambda x:-x[2])
         final_pred, final_inf, final_df = self.merge_preds(df_agg, df_in, predicates, g_att, g_agg, agg_method, target, dir)
+        new_df_agg = df_agg.copy()
+        new_df_agg[control] = final_df[control]
+        new_df_agg[target] = final_df[target]
         final_pred_by_attr = {}
-        for a, i in final_pred:
+        for a, i, rank in final_pred:
             if a not in final_pred_by_attr.keys():
                 final_pred_by_attr[a] = []    
-            final_pred_by_attr[a].append(i)
+            final_pred_by_attr[a].append((i, rank))
         fig, ax = plt.subplots(layout='constrained', figsize=(7, 7))
         x1 = list(df_agg.index)
         ind1 = np.arange(len(x1))
         y1 = df_agg.values
         
 
-        x2 = list(df.index)
+        x2 = list(final_df.index)
         ind2 = np.arange(len(x2))
         y2 = final_df.values
         if agg_method == 'count':
@@ -148,13 +158,19 @@ class OutlierMeasure(BaseMeasure):
         explanation = f'The predicate\n'
         for a, bins in final_pred_by_attr.items():
             first = bins[0]
-            t = type(first)
-            if type(bins[0]) is tuple:
+            t = type(first[0])
+            if t is tuple:
                 inter_exp = f'{a} between '
             else:
                 inter_exp = f'{a} is '
             for b in bins:
-                inter_exp += f'{b}, '
+                inter_exp += f'{b[0]}'
+                if b[1] is not None:
+                    if b[1] <= 5:
+                        inter_exp += f'- low'
+                    elif b[1] >= 15:
+                        inter_exp += f'- high'
+                inter_exp += f', '
             inter_exp += '\n'
             explanation += inter_exp
         explanation += 'has high influence on this outlier.'
